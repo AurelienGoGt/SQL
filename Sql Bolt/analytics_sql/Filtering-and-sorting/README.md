91642189861738f5522fa97092dbff97eae83598
Aurelien <aurelien.gorguet@outlook.com>
2026-01-25 22:54:58 +0200

    Lecon 3 termine
    Creation du readme.md + solutions.sql
    Affichage des explications

--- Sql Bolt/analytics_sql/Filtering-and-sorting/README.md
+++ Sql Bolt/analytics_sql/Filtering-and-sorting/README.md
@@ -1,1 +1,24 @@
-IN PROGRESS
+<h1 align="center">🎬 SQL Lesson 3 — Pixar Movies (Filtering & Sorting Results)</h1>
+
+<h2 align="center">Contexte de l’exercice :</h2>
+
+Dans cette leçon, la base Pixar est toujours utilisée, mais l’objectif est d’aller plus loin dans l’exploitation des résultats.
+Après avoir appris à filtrer les données avec **WHERE**, on cherche désormais à :
+
+- ordonner les résultats
+- limiter le nombre de lignes retournées
+
+<h2 align="center">Compétences apprises :</h2>
+
+- WHERE pour filtrer
+- ORDER BY pour trier
+- LIMIT pour restreindre
+
+<h2 align="center">Objectif de la leçon :</h2>
+
+À la fin de cet exercice, j’ai pu apprendre à :
+
+- trier des résultats SQL avec ORDER BY
+- contrôler l’ordre d’affichage des données (ASC / DESC)
+- limiter le nombre de résultats avec LIMIT
+- combiner filtrage, tri et limitation dans une même requête
---
+++ Sql Bolt/analytics_sql/Filtering-and-sorting/solutions.sql
@@ -0,0 +1,43 @@
+-- ======================================================
+-- SQLBolt – Select Queries
+-- Dataset: SQLBolt platform (conceptual)
+-- ======================================================
+
+-- Task 1 : List all directors of Pixar movies (alphabetically), without duplicates 
+-- Grain: Only unique director names in alphabetical order
+
+SELECT DISTINCT director
+FROM Movies
+ORDER BY Director ASC --- ASC is optional as default is ascending
+
+------------------------------------------------------
+
+-- Task 2 : List the last four Pixar movies released (ordered from most recent to least) 
+-- Grain: Only 4 rows of most recent Pixar movies
+
+SELECT title, year  --- you can use * to get all columns if you want too
+FROM Movies
+ORDER BY year DESC
+LIMIT 4
+
+------------------------------------------------------
+
+-- Task 3 : List the first five Pixar movies sorted alphabetically by title
+-- Grain: Only 5 rows of Pixar movies ordered by title ascending
+
+SELECT title
+FROM Movies
+ORDER BY title
+LIMIT 5
+
+------------------------------------------------------
+
+-- Task 4 : List the next five Pixar movies sorted alphabetically
+-- Grain: Only 5 rows of Pixar movies ordered by year ascending
+
+SELECT title, year
+FROM Movies
+ORDER BY Year ASC
+LIMIT 5 ;
+
+------------------------------------------------------
